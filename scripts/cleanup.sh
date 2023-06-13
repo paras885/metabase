60e95cb0595817ab198df75dfaa0733e4ca32103
Paras Kumar Meena <paras@Parass-MacBook-Pro.local>
2023-06-13 02:41:15 +0100

    Patch for cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Cleaning up the package files"
-if [ -d /var/lib/analytic-app-setup ]; then
-    rm -rf /var/lib/analytic-app-setup
+if [ -d /var/lib/analytic-app ]; then
+    rm -rf /var/lib/analytic-app
 fi
 echo "Cleaning up the init file"
-if [ -f /etc/init.d/analytic-app-setup ]; then
-    rm -f /etc/init.d/analytic-app-setup
+if [ -f /etc/init.d/analytic-app ]; then
+    rm -f /etc/init.d/analytic-app
 fi
